5808f5d341fdc372bafdbffbb0c8ba287da22307
РИШАТ Ахтямов <rishat-rus@mail.ru>
2023-11-20 15:11:02 +0400

    web

--- MultiAplicationForIS.WebUI/Pages/Index.cshtml.cs
+++ MultiAplicationForIS.WebUI/Pages/Index.cshtml.cs
@@ -1,20 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MultiAplicationForIS.BLForAPI;
+using MultiAplicationForIS.Core.Interfaces;
 
 namespace MultiAplicationForIS.WebUI.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IUserService _IUserService;
+
+        public string Message { get; private set; }
+
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _IUserService = new APIUserService();
         }
 
         public void OnGet()
         {
+           
+        }
+        public void OnPostLogIn(string login, string password)
+        {
+            try
+            {
+               var us =   _IUserService.GetUser(login, password);
+                if (us != null)
+                {
+                    Message = "Привет " + us.Name; 
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+
 
         }
+
+
+
     }
 }
