56e4dab6414ce870f7c6d54df3e2d9240a67c127
РИШАТ Ахтямов <rishat-rus@mail.ru>
2023-11-21 11:15:08 +0400

    sds

--- MultiAplicationForIS.AndroidUI/Platforms/Android/MainApplication.cs
+++ MultiAplicationForIS.AndroidUI/Platforms/Android/MainApplication.cs
@@ -6,6 +6,9 @@ namespace MultiAplicationForIS.AndroidUI
     [Application]
     public class MainApplication : MauiApplication
     {
+
+
+       
         public MainApplication(IntPtr handle, JniHandleOwnership ownership)
             : base(handle, ownership)
         {
